fafbe3213d91000d71a944e09d07c2be3863422a
Taska <77rex7@gmail.com>
2023-07-12 17:30:32 +0300

    update

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import telebot
-import webbrowser
+from telebot import types
 
 bot = telebot.TeleBot('6366976096:AAG-ouDXdOASxnB0WRuqeZf-BO3RLbrfeRQ')
 
 #add
-@bot.message_handler(commands=['site'])
-def site(message):
-    webbrowser.open('https://google.com')
+
+@bot.message_handler(commands=['minecraft'])
+def minecraft(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Запустить Сервер', callback_data='start_minecraft_server'))
+    markup.add(types.InlineKeyboardButton('Перезапустить Сервер', callback_data='restart_minecraft_server'))
+    markup.add(types.InlineKeyboardButton('Остановить Сервер', callback_data='stop_minecraft_server'))
+    bot.reply_to(message, 'Мониторинг состояния сервера Minecraft \n \n Статус: Active/Sleep/Inactive \n Версия: NaN \n Игроков: N/N \n Игроки: NaN \n \n Управление сервером:', reply_markup = markup)
+
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
@@ -18,6 +25,12 @@ def info(message):
         bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'ID: {message.from_user.id}')
+
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'start_minecraft_server':
+        None #TODO
 
 
 bot.polling(none_stop=True)
